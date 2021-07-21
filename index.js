5aa2cc563525e909760a953b5e3e00e3d2bf8b6b
andres <andres.gordillo@mail.citytech.cuny.edu>
2021-07-21 18:05:23 -0400

    commit tree 8e00290de05150823140b7d1522b249b96115fb8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { stringContaining } = require("expect");
+
 // Code your solution here
+function findMatching(drivers,string){
+    let answer =  drivers.filter(el => el.toLowerCase() === string.toLowerCase());
+
+   return answer;
+
+}
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(el => el.startsWith(string));
+}
+
+function matchName(drivers, string){
+    return drivers.filter(el => el.name === string);
+}
